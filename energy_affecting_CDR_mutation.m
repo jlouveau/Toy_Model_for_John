53c84fab9deba5444f8fe8672529c75aac58c936
jlouveau <jlouveau@dhcp-18-111-78-141.dyn.MIT.EDU>
2016-12-05 17:23:12 -0500

    k to kappa in CDR_mutations

--- energy_affecting_CDR_mutation.m
+++ energy_affecting_CDR_mutation.m
@@ -1,4 +1,4 @@
-function [ mutant ] = energy_affecting_CDR_mutation( wildtype, k, sigma, mu)
+function [ mutant ] = energy_affecting_CDR_mutation( wildtype, kappa, sigma, mu)
 %   If the wildtype cell saw the first antigen in the previous round of
 %   selection (or was selected to become founder), wildtype(nb_Ag+2) = 1.
 %   Then Ev1 and Ec are increased by values randomly chosen from the skewed
@@ -8,12 +8,12 @@ function [ mutant ] = energy_affecting_CDR_mutation( wildtype, k, sigma, mu)
 mutant = wildtype;
 
 if wildtype(length(mutant)) == 1
-    mutant(1) = mutant(1) + gevrnd(k, sigma, mu, 1, 1);
+    mutant(1) = mutant(1) + gevrnd(kappa, sigma, mu, 1, 1);
     mutant(2) = rand;
-    mutant(3) = mutant(3) + gevrnd(k, sigma, mu, 1, 1);
+    mutant(3) = mutant(3) + gevrnd(kappa, sigma, mu, 1, 1);
 else
     mutant(1) = rand;
-    mutant(2) = mutant(2) + gevrnd(k, sigma, mu, 1, 1);    
-    mutant(3) = mutant(3) + gevrnd(k, sigma, mu, 1, 1);
+    mutant(2) = mutant(2) + gevrnd(kappa, sigma, mu, 1, 1);    
+    mutant(3) = mutant(3) + gevrnd(kappa, sigma, mu, 1, 1);
 end
 end
